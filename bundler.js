311faaecfff7162e07052a898b0bd4c5c28ac408
Nathan Lapinski <n4t311@gmail.com>
2018-07-17 16:52:08 +0900

    finalized v1

--- bundler.js
+++ bundler.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 const babylon = require('babylon');
+const babel = require('babel-core');
 const traverse = require('babel-traverse').default;
 
 let ID = 0;
@@ -27,12 +29,75 @@ function createAsset(filename) {
 
     const id = ID++;
 
+    const {code} = babel.transformFromAst(ast, null, {
+        presets: ['env']
+    });
+
     return {
         id,
         filename,
-        dependencies
+        dependencies,
+        code
     };
 }
 
-const mainAsset = createAsset('./example/entry.js');
-console.log(mainAsset);
+// TODO: Circular deps?
+function depsGraphBuilder(entryPoint) {
+    const mainAsset = createAsset(entryPoint);
+    let depsGraph = [mainAsset];
+
+    for (const asset of depsGraph) {
+        const dirname = path.dirname(asset.filename);
+        asset.mapping = {};
+
+        asset.dependencies.forEach(relativePath => {
+            const absPath = path.join(dirname, relativePath);
+            const child = createAsset(absPath);
+            asset.mapping[relativePath] = child.id;
+            depsGraph.push(child);
+        });
+    }
+
+    return depsGraph;
+}
+
+function bundle(graph) {
+    let modules = '';
+
+    graph.forEach(node => {
+        modules += `${node.id}: [
+            function (require, module, exports) {
+                ${node.code}
+            },
+            ${JSON.stringify(node.mapping)}
+        ],`
+    });
+
+    const result = `
+        (function(modules){
+            function require(id) {
+                const [fn, mapping] = modules[id];
+
+                function localRequire(relPath) {
+                    return require(mapping[relPath]);
+                }
+
+                const module = {
+                    exports: {}
+                };
+
+                fn(localRequire, module, module.exports);
+
+                return module.exports;
+            }
+
+            require(0);
+        })({${modules}})
+    `;
+
+    return result;
+}
+
+const graph = depsGraphBuilder('./example/entry.js');
+const result = bundle(graph);
+console.log(result);
